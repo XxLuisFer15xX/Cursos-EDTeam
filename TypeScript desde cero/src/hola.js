624f1c8b640b7475283887a64246dc817850e118
Luis <luisfer.sm15@gmail.com>
2020-07-13 11:03:48 -0600

    TypeScript desde cero

---
+++ TypeScript desde cero/src/hola.js
@@ -0,0 +1,4 @@
+var saludo = 'Bienvenidos a TypeScript';
+console.log(saludo);
+var fecha = '11/07/2020';
+console.log('fecha', fecha);
---
+++ TypeScript desde cero/src/hola.ts
@@ -0,0 +1,5 @@
+let saludo = 'Bienvenidos a TypeScript';
+console.log(saludo);
+
+let fecha = '11/07/2020';
+console.log('fecha', fecha);
